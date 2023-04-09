060f3946af4d8e368f83ac30df06bab7ba84a1a8
Dok2314 <koversundanil@gmail.com>
2023-04-09 21:32:48 +0300

    Added class active to link

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -4,20 +4,20 @@ import navCss from "./Navigation.module.css";
 const Nav = () => {
     return (
         <nav className={navCss.nav}>
-            <div className={navCss.item}>
-                <NavLink to={'profile'}>Profile</NavLink>
+            <div>
+                <NavLink to={'profile'} className={ navData => navData.isActive ? navCss.active : navCss.item }>Profile</NavLink>
             </div>
-            <div className={navCss.item}>
-                <NavLink to={'dialogs'}>Messages</NavLink>
+            <div>
+                <NavLink to={'dialogs'} className={ navData => navData.isActive ? navCss.active : navCss.item }>Messages</NavLink>
             </div>
-            <div className={navCss.item}>
-                <a href="">News</a>
+            <div>
+                <NavLink to={'news'} className={ navData => navData.isActive ? navCss.active : navCss.item }>News</NavLink>
             </div>
-            <div className={navCss.item}>
-                <a href="">Music</a>
+            <div>
+                <NavLink to={'music'} className={ navData => navData.isActive ? navCss.active : navCss.item }>Music</NavLink>
             </div>
-            <div className={navCss.item}>
-                <a href="">Settings</a>
+            <div>
+                <NavLink to={'settings'} className={ navData => navData.isActive ? navCss.active : navCss.item }>Settings</NavLink>
             </div>
         </nav>
     );
