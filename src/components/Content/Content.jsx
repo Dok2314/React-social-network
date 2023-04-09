509ab6e995c38b99f3892a74381afb3195a0c58f
Dok2314 <koversundanil@gmail.com>
2023-04-09 17:05:59 +0300

    First commit

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,28 @@
+const Content = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
+            </div>
+
+            <div>
+                ava + description
+            </div>
+
+            <div>
+                My Posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                    <div>Post 3</div>
+                    <div>Post 4</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/488px-Apple_logo_black.svg.png" alt=""/>
+        </header>
+    );
+};
+
+export default Header;
