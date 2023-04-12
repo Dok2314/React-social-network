a0acf9719154ef17d52e98382ac97299942ea018
Dok2314 <koversundanil@gmail.com>
2023-04-12 21:00:19 +0300

    Changes

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,31 @@ import dialogsCss from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={dialogsCss.dialogs}>
+            <div className={dialogsCss.dialogsItems}>
+                <div className={`${dialogsCss.dialog} ${dialogsCss.active}`}>
+                    Daniil
+                </div>
+                <div className={dialogsCss.dialog}>
+                    Sabina
+                </div>
+                <div className={dialogsCss.dialog}>
+                    Oleg
+                </div>
+                <div className={dialogsCss.dialog}>
+                    Sveta
+                </div>
+                <div className={dialogsCss.dialog}>
+                    Nika
+                </div>
+            </div>
+
+            <div className={dialogsCss.messages}>
+                <div className={dialogsCss.message}>Hi</div>
+                <div className={dialogsCss.message}>How is your dok?</div>
+                <div className={dialogsCss.message}>Hello dude!</div>
+                <div className={dialogsCss.message}>My name John!</div>
+            </div>
         </div>
     );
 };
